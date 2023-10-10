b4a9f2f83b15369287e88c5447096cf1ebe84979
Uživatel <Uivatel@DELL_9020_1>
2023-10-10 17:03:02 +0200

    Přidejte soubory projektu.

---
+++ Prikazy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZmenyPole
+{
+    static public class Prikazy
+    {
+        public static int[] UpravPole(this int[] pole)
+        {
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (pole[i] < 0) pole[i] = Math.Abs(pole[i]); 
+                else { if (pole[i] == 0) pole[i] = i; }
+            }
+            Console.WriteLine("Hodnoty v poli byly upraveny.");
+            return (pole);
+        }
+        public static void VypisPole(this int[] pole)
+        {
+            foreach (int i in pole) Console.Write(i + ", ");
+            Console.WriteLine();
+        }
+
+        public static int maxCislo(this int[] pole)
+        {
+            int maxCislo = pole[0];
+            foreach (int k in pole)
+            {
+                if (k > maxCislo) maxCislo = k;
+            }
+            return maxCislo;
+        }
+        public static int[] SetridVzestupne(this int[] pole)
+        {
+            int maxCislo = pole.maxCislo();
+            int[] novePole = new int[pole.Length];
+            int index = 0;
+
+            for (int k = 0; k < novePole.Length; k++)
+            {
+                int minimalni = pole[0];
+
+                for (int i = 0; i < pole.Length; i++)
+                {
+                    if (pole[i] <= minimalni)
+                    {
+                        minimalni = pole[i];
+                        index = i;
+                    }
+                }
+                pole[index] = maxCislo+1;
+                novePole[k] = minimalni;
+            }
+
+            Console.WriteLine("Setřídění bylo dokončeno.");
+            return novePole;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZmenyPole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mojePole = { 3, -4, 0, 21, 3, 16, 7, 0, 1, 3, 4, -2 };
+            mojePole.VypisPole();
+            Console.WriteLine();
+            mojePole.UpravPole();
+            mojePole.VypisPole();
+            Console.WriteLine();
+            Console.WriteLine("Největší číslo: " + mojePole.maxCislo());
+            Console.WriteLine();
+            VypisPole(mojePole.SetridVzestupne());
+            Console.ReadLine();
+        }
+        public static void VypisPole(int[] pole)
+        {
+            foreach (int i in pole) Console.Write(i + ", ");
+            Console.WriteLine();
+        }
+}
+}
